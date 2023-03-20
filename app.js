28a2db70a3bf1a3f9a9fb856747e304797d7ffbd
ForwardOoh <soa9719@naver.com>
2023-03-20 21:46:33 +0900

    public 및 css연동 추가

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const app = express();
+
 const adminRoutes = require("./routes/admin");
 const shopnRoutes = require("./routes/shop");
 
-const app = express();
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(shopnRoutes);

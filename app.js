8b8fd01ff4ac0296f0fbc30205010a20e5065d26
ForwardOoh <soa9719@naver.com>
2023-03-21 16:59:33 +0900

    pug 추가

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopnRoutes = require("./routes/shop");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/admin", adminData.routes);
 app.use(shopnRoutes);
-app.use(adminRoutes);
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, "views", "not-found.html"));
